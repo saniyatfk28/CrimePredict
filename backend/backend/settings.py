4ee3f753479d783dcc6b432801bab74785fecbff
Saniyat Firoz Khan <100563522+saniyatfk28@users.noreply.github.com>
2026-06-13 17:59:25 +0600

    Update settings.py (removed smtp credentials)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,12 +161,12 @@ EMAIL_MODE = "smtp"   # change to "console" for demo fallback
 if EMAIL_MODE == "smtp":
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
+    EMAIL_HOST = # use your own
+    EMAIL_PORT = # use your own
     EMAIL_USE_TLS = True
 
-    EMAIL_HOST_USER = "saniyatfirozkhan28@gmail.com"
-    EMAIL_HOST_PASSWORD = "quri hfdp xrpr faqm"
+    EMAIL_HOST_USER = # use your own
+    EMAIL_HOST_PASSWORD = # use your own app password
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     
